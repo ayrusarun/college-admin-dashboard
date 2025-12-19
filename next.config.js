bd0059f28c6f1afb6c01c42fb418b9bb78ec53e4
suryr <suryr@paypal.com>
2025-12-19 23:16:08 +0530

    teS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    turbo: {
+      root: '/app',
+    },
+  },
 };
 
 module.exports = nextConfig;
